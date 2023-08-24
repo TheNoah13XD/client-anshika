182a07d4c803e60d280222e57a4fda1958137191
TheNoah13XD <broken.personal.1211@gmail.com>
2023-08-25 00:38:17 +0530

    work page changes

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -22,6 +22,7 @@ const WorkPage: FC = ({}) => {
                         <div className="definition-1">
                             <p>where style dazzles, fashion redefines allure.</p>
                             <img src="/shapes/2.png" alt="two" />
+                            <IconButton icon="north_east" />
                         </div>
                         <div className="definition-2">
                             <p>Captivating style redefines allure with enchanting fashion, unveiling limitless elegance.</p>
