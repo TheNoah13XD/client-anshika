7289eed6cd17ea4fe4444348738aee921d67e9e6
TheNoah13XD <broken.personal.1211@gmail.com>
2023-08-25 03:40:25 +0530

    human page base

--- src/app/human/page.tsx
+++ src/app/human/page.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 
+// components
+import Button from "@/components/ui/Button";
+import IconButton from "@/components/ui/IconButton";
+
 interface HumanPageProps {
 
 }
@@ -7,7 +11,25 @@ interface HumanPageProps {
 const HumanPage: FC<HumanPageProps> = ({}) => {
     return (
         <>
-            
+            <div className="human-container">
+                <div className="basics">
+                    <p className="brand">Anshika</p>
+                    <p className="tagline">REVAMP, INSPIRE</p>
+                    <Button icon="arrow_forward">Work</Button>
+                </div>
+                <div className="work-around">
+                    <p>Empowering fashion expression with curated expertise.</p>
+                    <img src="/shapes/5.png" alt="five" />
+                    <IconButton icon="north_east" />
+                </div>
+                <div className="redirect">
+                    <p>Discover a captivating portfolio of our remarkable work. </p>
+                    <div>
+                        <p>Explore our creative vision.</p>
+                        <IconButton icon="north_east" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
